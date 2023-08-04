1399979ac838239da87518767e7e05b655eeeade
Iamtheusername112 <apkfem@gmail.com>
2023-08-04 12:11:51 +0200

    Challange solved

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const phonesData = require("./data/phones.json");
+
+app.get("/phones", (req, res) => {
+  res.json(phonesData);
+});
+
+app.get("/phones/:id", (req, res) => {
+  const phoneId = parseInt(req.params.id, 10);
+  const phone = phonesData.find((phone) => phone.id === phoneId);
+  if (!phone) {
+    return res.status(404).json({ message: "Phone not found" });
+  }
+  res.json(phone);
+});
+
+const PORT = 7000;
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
